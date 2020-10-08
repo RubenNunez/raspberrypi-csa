313a7776e22f86a7f9f65a463fdb5264ea1e702f
Ruben Nunez <Ruben.Nunez@live.de>
2020-10-09 00:26:45 +0200

    init commit

--- CSA_GAME/Program.cs
+++ CSA_GAME/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+// ReSharper disable InconsistentNaming
 
 namespace CSA_GAME
 {
     class Program
     {
+        private const int TIME_WAITING_FOR_DEBUGGER = 25;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var time = 0;
+            Console.WriteLine("waiting for debugger to attach: ");
+            Thread.Sleep(5 * 1000);
+            while (!Debugger.IsAttached & time < TIME_WAITING_FOR_DEBUGGER)
+            {
+                Console.Write("▉");
+                time++;                    
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Prg End");
+
+            var explorer700 = new Explorer700.Explorer700();
+
+            for (var i = 0; i < 7; i++)
+            {
+                explorer700.Led1.Toggle();
+                explorer700.Led2.Toggle();
+                Thread.Sleep(250);
+            }
         }
     }
 }
---
+++ CSA_GAME/Tools/deploy.bat
@@ -0,0 +1,4 @@
+﻿C:\Windows\System32\OpenSSH\scp.exe -r $(TargetDir) pi@raspberrypi:/home/pi/apps/$(ProjectName)
+
+$(SolutionDir)SecureUpload.exe $(TargetDir) pi-home:pi/apps/$(ProjectName)
+#scp -r $(TargetDir) pi@raspberrypi:/home/pi/apps/$(ProjectName)
